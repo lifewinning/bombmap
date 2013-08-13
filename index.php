ebf57cbff06df9efd5d78aa8eaf3604e678b9a76
lifewinning <lifewinning@gmail.com>
2013-08-13 09:46:44 -0400

    intial commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Template Name: downtown
+*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>
+      BOMB's Downtown Guide
+    </title>
+  <head>
+    <meta charset='utf-8' />
+    <link href='ext/mapbox.css' rel='stylesheet' />
+   
+<link rel="stylesheet" type="text/css" href="http://bombsite.powweb.com/wp-content/themes/sandbox/style.css" />   
+    <style>
+      #map { 
+        position:absolute; top:0;bottom:0; left: -1; width:100%;z-index:-1;
+      }  
+
+      .marker-tooltip h1{
+        font-family: sans-serif;
+        font-size: 18px;
+        font-weight: 800;
+      } 
+
+      .marker-tooltip p {
+        font-size: 12px;
+        font-family: sans-serif;
+        font-weight: 200;
+      }
+      .marker-tooltip a{
+        text-decoration: none;
+        color: #ff00b1;
+      }
+      .marker-popup{
+        -moz-border-radius: 10%;
+        border-radius: 10%;
+
+
+      }
+
+
+
+</style>
+  </head>
+  <body>
+
+<!-- <div class="controls" style="z-index: 0;">
+      <div id="guidelink">
+        <div class="img-shadow">
+          <div id="mapContainer">
+            <h1>Downtown Galleries</h1>
+            
+          </div>
+        </div>
+      </div>
+</div> -->
+
+<!-- Section: Map -->
+   
+<div id='map'>
+</div>
+  </body>
+</html>
+  
+    <script src='https://www.google.com/jsapi'></script>
+    <script src='http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js'></script>
+    <script src='ext/jquery.min.js'></script>
+    <script src='ext/underscore.min.js'></script>
+    <script src='ext/google_docs.js'></script>
+    <script src='site.js'></script>
+  </body>
+</html>
---
+++ site.js
@@ -0,0 +1,89 @@
+
+// declare your spreadsheet ID and basemap
+
+var data_id = '0AkuokbMVYNRxdEFPeFBZdkY0S05CWUZZcWFfaFpfeGc',
+    sheet = 'od6',
+    //map_id = 'lifewinning.map-7nibzgui',
+    markerLayer,
+    features,
+    features_summary,
+    interaction,
+    m = mapbox.map('map');
+    //layer = mapbox.layer().id(map_id),
+    //map = mapbox.map('map', layer, null, [easey_handlers.DragHandler()]);
+
+
+//add stamen tiles
+
+m.addLayer(new MM.TemplatedLayer('http://b.tile.stamen.com/toner-lite/{Z}/{X}/{Y}.png'));
+m.ui.attribution.add()
+    .content('Map tiles by <a href="http://stamen.com">Stamen Design</a>, under' +
+    ' <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' +
+    ' Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.');
+m.centerzoom({
+ lat: 40.72189,
+        lon: -73.99927
+        }, 15);
+m.setZoomRange(12, 18);
+//m.ui.attribution.add().content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');
+
+
+// center and zoom your map
+mmg_google_docs_spreadsheet(data_id, sheet, mapData);
+
+// Build map
+function mapData(f) {
+    features = f;
+    markerLayer = mapbox.markers.layer().features(features);
+
+
+  //center markers layer
+    markerLayer.factory(function (n) {
+        // Create a marker using the simplestyle factory
+        var elem = mapbox.markers.simplestyle_factory(n);
+        // Add function that centers marker on click
+        MM.addEvent(elem, 'click', function(e) {
+            var markerwindow = $('.marker-popup').height();
+            console.log(markerwindow);
+            m.ease.location({
+              lat: n.geometry.coordinates[1]+(markerwindow/100000),
+              lon: n.geometry.coordinates[0]
+            }).zoom(m.zoom()).optimal();
+        
+        });
+        return elem;
+
+
+    });
+
+    interaction = mapbox.markers.interaction(markerLayer);
+    m.addLayer(markerLayer);
+    m.ui.zoomer.add();
+    m.ui.zoombox.add();
+
+    interaction.formatter(function (feature) {
+        var o = '<h1>' + feature.properties.name + '</h1><p>';
+        
+        var p = '<p style=text-align:center;><img src=' + feature.properties.image + ' style= max-width:60%;></p>';
+        
+        var q = feature.properties.address + '<br><a href=http://'+ feature.properties.website + '>'+ feature.properties.website + '</a><br/>'+ feature.properties.phone + '</p>';
+        
+        var r = '<hr><p>' + feature.properties.schedule + '</p>';
+
+        var color = feature.properties.color;
+
+        if (color=='pink'){
+        return o + p+  q + r;
+        }
+        else{
+            return o + q;
+        }
+       
+    });
+
+}
+
+ 
+
+
+
